f2bf21d7ffc0f4582731a2f7cef21d9bd35eef92
hieutrungdang <danghieu1995@gmail.com>
2021-09-23 00:38:27 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 
+define PI 3.14
+
 int main() {
   cout << "Hello world!" << endl;
   cout << "See ya!" << endl;
+  cout << "PI = " << PI << endl;
+  
   
   return 0;
 }
